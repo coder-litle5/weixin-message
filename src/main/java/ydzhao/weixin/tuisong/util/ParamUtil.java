554e84f960a6bc51f5b68e4694529b77fca6cd8c
wushenglin <wsl.20130215>
2022-08-25 16:29:24 +0800

    参数

---
+++ src/main/java/ydzhao/weixin/tuisong/util/ParamUtil.java
@@ -0,0 +1,30 @@
+package ydzhao.weixin.tuisong.util;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class ParamUtil {
+
+    //推送的微信用户openid
+    public static String wsl = "ohCeC6tWk76hN2K4RhNaPqZjq3LE";
+    public static String rl = "ohCeC6s4RkxrYjRHAtHTeNaLzgDo";
+
+    //微信测试号的appid和secretid和模板id
+    public static String appId = "wxb15868b1ebc0af6f";
+    public static String secret = "3618641d88a596c020d69a3c0a0300df";
+    public static String am_templateId = "SFzzN-f9jktSKVnemxrVPU1Wt9A2hXT_kvjgKmn577E";
+
+    //百度申请的ak
+    public static String ak = "Qv7bgoVk8DbR02lTL79kmxpt4FPY2sRI";
+    //地区的编码
+    public static String district_id = "510100";
+
+    //彩虹屁接口的key
+    public static String key = "d78e5ae9dd1393673d6cc903b16d1140";
+
+    public static String txturl_test="G:\\idea-projects\\weixin-message\\src\\main\\resources\\jinju.txt";
+    public static String txturl="E:\\Tomcat 8.5_wexinpush\\webapps\\ROOT\\WEB-INF\\classes\\jinju.txt";
+
+
+}
